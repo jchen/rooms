ab42e011ed77a3c441c039e018c7f5f3dbfb61f5
Jiahua Chen <33486899+jchen@users.noreply.github.com>
2024-01-31 11:22:23 -0500

    Room 210 alert

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,14 @@ export default class App extends React.Component<{}> {
             <Alert variant="info">
               Welcome to <b>rooms.cs.brown.edu</b>. You can view room available within
               the CIT using this tool. Please direct
-              bookings to <a href="mailto:mta@cs.brown.edu">mta@cs.brown.edu</a>
-              ,{" "}
+              bookings to <a href="mailto:mta@cs.brown.edu">mta@cs.brown.edu</a>,{" "}
               <a href="mailto:reception@cs.brown.edu">reception@cs.brown.edu</a>
               , or the Brown Scheduler/Registrar depending on the room type as
               specified by hovering over the tag in bold. Clicking on a room
               will link to the room's Google Calendar.
+            </Alert>
+            <Alert variant="danger">
+              We are aware that the calendar for <b>CIT Room 210</b> is out of order. Your room bookings might not show on this calendar. You should verify with the Google calendar of this room that it is available for your specific booking. Please email us if you have any questions. Thanks!
             </Alert>
           </Row>
           <Row className="justify-content-lg-center">
